fa535ea1be6789c60ad1d4f1e207a778ba03e511
LimnedMoonlight <limnedmoonlight@gmail.com>
2026-03-18 05:15:29 +0100

    Update memoryOptimization.lua

--- src/memoryOptimization.lua
+++ src/memoryOptimization.lua
@@ -30,7 +30,7 @@ function memoryOptimization:update(dt)
         local ramAfter = collectgarbage("count")
         local cleaned = ramBefore - ramAfter
         
-        if dbg then Logging.warning(string.format("[FS25_memoryOptimization] Gründliche 12-Min-Bereinigung: %.2f KB freigegeben | Aktueller RAM verfügbar: %.2f KB", cleaned, ramAfter)) end
+        if dbg then Logging.warning(string.format("[FS25_memoryOptimization] Gründliche 12-Min-Bereinigung: %.2f KB freigegeben | Aktueller Speicher verfügbar: %.2f KB", cleaned, ramAfter)) end
         
         timer = 0
         nextMedium = MEDIUM_INTERVAL -- Reset für die 3-Minuten-Schwellen
@@ -44,7 +44,7 @@ function memoryOptimization:update(dt)
         local ramAfter = collectgarbage("count")
         local cleaned = ramBefore - ramAfter
         
-        if dbg then Logging.warning(string.format("[FS25_memoryOptimization] 3-Min-Zwischenbereinigung: %.2f KB freigegeben | Aktueller RAM verfügbar: %.2f KB", cleaned, ramAfter)) end
+        if dbg then Logging.warning(string.format("[FS25_memoryOptimization] 3-Min-Zwischenbereinigung: %.2f KB freigegeben | Aktueller Speicher verfügbar: %.2f KB", cleaned, ramAfter)) end
         nextMedium = nextMedium + MEDIUM_INTERVAL -- Schwellenwert um 3 Min erhöhen
     end
 end
