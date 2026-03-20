10f029ce29a3023e6a259e07e9167956f8437bd9
LimnedMoonlight <limnedmoonlight@gmail.com>
2026-03-20 14:19:22 +0100

    Update memoryOptimization.lua

--- src/memoryOptimization.lua
+++ src/memoryOptimization.lua
@@ -1,56 +1,45 @@
 -- ===========================================================================
+--
 -- memoryOptimization.lua
 --
 -- Autor: LimnedMoonlight
--- Version: 1.0.0.5
+-- Version: 1.0.0.2
 -- ===========================================================================
 -- Changelog:
 --   1.0.0.0 - Initial Release
---   1.0.0.5 - changed Intervalls to 3 / 12 Minutes
+--
+--   1.0.0.1 - Changed the Prints and comments to en language
+--
+--   1.0.0.2 - changed to one Intervall every 5 Minutes.
+--
 -- ===========================================================================
 
 memoryOptimization = {}
 
 local timer = 0
-local MEDIUM_INTERVAL = 180000 -- 3 Minutes in Milliseconds
-local FULL_INTERVAL = 720000   -- 12 Minutes in Milliseconds
-local nextMedium = MEDIUM_INTERVAL 
+local MEDIUM_INTERVAL = 300000 -- 5 Minutes in Milliseconds
+local nextMedium = MEDIUM_INTERVAL
 local dbg = true
 
 function memoryOptimization:update(dt)
     timer = timer + dt
 
-    if timer >= FULL_INTERVAL then
+    -- Cleaning-Routine
+    if timer >= nextMedium then
         local ramBefore = collectgarbage("count")
-        
-        -- Double Cleanup
-        collectgarbage("collect")
-        collectgarbage("collect") 
-        
-        local ramAfter = collectgarbage("count")
-        local cleaned = ramBefore - ramAfter
-        
-        if dbg then Logging.warning(string.format("[FS25_memoryOptimization] Thorough 12-minute cleanup: %.2f KB freed | Current available memory: %.2f KB", cleaned, ramAfter)) end
-        
-        timer = 0
-        nextMedium = MEDIUM_INTERVAL -- Reset for 3-Minute interim
 
-    -- 3-Minute Routine
-    elseif timer >= nextMedium then
-        local ramBefore = collectgarbage("count")
-        
         collectgarbage("collect")
-        
+
         local ramAfter = collectgarbage("count")
         local cleaned = ramBefore - ramAfter
-        
-        if dbg then print(string.format("[FS25_memoryOptimization] 3-minute interim cleanup: %.2f KB freed | Current available memory: %.2f KB", cleaned, ramAfter)) end
-        nextMedium = nextMedium + MEDIUM_INTERVAL -- Schwellenwert um 3 Min erhöhen
+
+        if dbg then print(string.format("[FS25_memoryOptimization] interim cleanup: %.2f KB freed | Current available memory: %.2f KB", cleaned, ramAfter)) end
+        nextMedium = nextMedium + MEDIUM_INTERVAL -- Count to next interim
     end
 end
 
 function memoryOptimization:loadMap(name)
-    Logging.warning("[FS25_memoryOptimization] Mod aktiv - Intervalle: 3 Min / 12 Min")
+    if dbg then print("[FS25_memoryOptimization] Mod active - Intervalls: 5 Minutes") end
     timer = 0
     nextMedium = MEDIUM_INTERVAL
 end
